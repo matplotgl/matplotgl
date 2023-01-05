68083b5c8b4fc7ca0f96053459685c416fb93e8c
Neil Vaytet <neil.vaytet@esss.se>
2023-01-05 23:18:16 +0100

    update

--- src/matplotgl/axes.py
+++ src/matplotgl/axes.py
@@ -4,62 +4,80 @@
 from .transform import Transform
 from .utils import value_to_string
 
+import ipywidgets as ipw
 import pythreejs as p3
 from matplotlib import ticker
 import numpy as np
 from typing import List, Tuple
 
+# def _make_sprite(string: str,
+#                  position: Tuple[float, float, float],
+#                  color: str = "black",
+#                  size: float = 1.0) -> p3.Sprite:
+#     """
+#     Make a text-based sprite for axis tick.
+#     """
+#     sm = p3.SpriteMaterial(map=p3.TextTexture(string=string,
+#                                               color=color,
+#                                               size=300,
+#                                               squareTexture=False),
+#                            transparent=True)
+#     return p3.Sprite(material=sm, position=position, scale=[size, size, size])
 
-def _make_sprite(string: str,
-                 position: Tuple[float, float, float],
-                 color: str = "black",
-                 size: float = 1.0) -> p3.Sprite:
-    """
-    Make a text-based sprite for axis tick.
-    """
-    sm = p3.SpriteMaterial(map=p3.TextTexture(string=string,
-                                              color=color,
-                                              size=300,
-                                              squareTexture=False),
-                           transparent=True)
-    return p3.Sprite(material=sm, position=position, scale=[size, size, size])
+
+def _make_outline_array(xticks, yticks, tick_size=0.02):
+    x = [0]
+    for yt in yticks:
+        x += [0, tick_size, 0]
+    x += [0, 1, 1]
+    for xt in xticks[::-1]:
+        x += [xt, xt, xt]
+    x += [0]
+
+    y = [0]
+    for yt in yticks:
+        y += [yt, yt, yt]
+    y += [1, 1, 0]
+    for xt in xticks[::-1]:
+        y += [0, tick_size, 0]
+    y += [0]
+    return np.array([x, y, np.zeros_like(x)], dtype='float32').T
 
 
-def _make_outline(color='black'):
-    geom = p3.BufferGeometry(
-        attributes={
-            'position':
-            p3.BufferAttribute(array=np.array(
-                [[0, 0, 1, 1, 0], [0, 1, 1, 0, 0], [0, 0, 0, 0, 0]],
-                dtype='float32').T),
-        })
+def _make_outline(xticks, yticks, color='black', tick_size=0.02):
+    array = _make_outline_array(xticks=xticks,
+                                yticks=yticks,
+                                tick_size=tick_size)
+    geom = p3.BufferGeometry(attributes={
+        'position': p3.BufferAttribute(array=array),
+    })
     material = p3.LineBasicMaterial(color=color, linewidth=1)
     return p3.Line(geometry=geom, material=material)
 
 
-def _make_frame(color='white'):
-    dx = 1.0
-    x = np.array([-dx, -dx, 1 + dx, 1 + dx, 0, 0, 1, 1])
-    y = np.array([-dx, 1 + dx, 1 + dx, -dx, 0, 1, 1, 0])
+# def _make_frame(color='white'):
+#     dx = 1.0
+#     x = np.array([-dx, -dx, 1 + dx, 1 + dx, 0, 0, 1, 1])
+#     y = np.array([-dx, 1 + dx, 1 + dx, -dx, 0, 1, 1, 0])
 
-    vertices = np.array([x, y, np.zeros_like(x)], dtype='float32').T
+#     vertices = np.array([x, y, np.zeros_like(x)], dtype='float32').T
 
-    faces = np.asarray(
-        [[0, 4, 1], [1, 4, 5], [1, 5, 2], [2, 5, 6], [2, 6, 3], [3, 6, 7],
-         [0, 3, 7], [0, 7, 4]],
-        dtype='uint16').ravel()  # We need to flatten index array
+#     faces = np.asarray(
+#         [[0, 4, 1], [1, 4, 5], [1, 5, 2], [2, 5, 6], [2, 6, 3], [3, 6, 7],
+#          [0, 3, 7], [0, 7, 4]],
+#         dtype='uint16').ravel()  # We need to flatten index array
 
-    geom = p3.BufferGeometry(attributes=dict(
-        position=p3.BufferAttribute(vertices, normalized=False),
-        index=p3.BufferAttribute(faces, normalized=False),
-    ))
+#     geom = p3.BufferGeometry(attributes=dict(
+#         position=p3.BufferAttribute(vertices, normalized=False),
+#         index=p3.BufferAttribute(faces, normalized=False),
+#     ))
 
-    return p3.Mesh(geometry=geom,
-                   material=p3.MeshBasicMaterial(color=color),
-                   position=[0, 0, -1])
+#     return p3.Mesh(geometry=geom,
+#                    material=p3.MeshBasicMaterial(color=color),
+#                    position=[0, 0, -1])
 
 
-class Axes(p3.Group):
+class Axes:
 
     def __init__(self) -> None:
 
@@ -71,17 +89,28 @@ class Axes(p3.Group):
         self._transformy = Transform()
         self._fig = None
         self._artists = []
+        self._width = 100
+        self._height = 100
+        self.font_size = 14
 
-        self._outline = _make_outline()
-        self._frame = _make_frame()
+        xticklabels, xticks = self._make_xticks(transform=self._transformx,
+                                                width=self._width)
+        yticklabels, yticks = self._make_yticks(transform=self._transformy,
+                                                height=self._height)
+        self._outline = _make_outline(xticks=xticks, yticks=yticks)
 
-        limits = [[self.xmin, self.xmax], [self.ymin, self.ymax], [0, 0]]
-        self._tick_size = 0.05
-        self.ticks = self._make_ticks(limits=limits)
+        self._left_bar = ipw.HTML(yticklabels)
+        self._right_bar = ipw.HTML()
+        self._bottom_bar = ipw.HTML(xticklabels)
+        self._top_bar = ipw.HTML()
+        # self._frame = _make_frame()
 
-        super().__init__()
-        for obj in (self._outline, self.ticks, self._frame):
-            self.add(obj)
+        # limits = [[self.xmin, self.xmax], [self.ymin, self.ymax], [0, 0]]
+        # self.ticks = self._make_ticks(limits=limits)
+
+        # super().__init__()
+        # for obj in (self._outline, self.ticks, self._frame):
+        #     self.add(obj)
 
     def plot(self, *args, **kwargs):
         from .plot import plot as p
@@ -110,25 +139,51 @@ class Axes(p3.Group):
     def get_figure(self):
         return self._fig
 
-    def _make_ticks(self, limits, ndim=2) -> p3.Group:
+    def _make_xticks(self, transform, width) -> str:
         """
         Create tick labels on outline edges
         """
-        ticks_group = p3.Group()
-        iden = np.identity(3, dtype=np.float32)
-        ticker_ = ticker.MaxNLocator(5)
-        transforms = [self._transformx, self._transformy]
-        for axis in range(ndim):
-            ticks = ticker_.tick_values(limits[axis][0], limits[axis][1])
-            for tick in ticks:
-                if limits[axis][0] <= tick <= limits[axis][1]:
-                    tick_pos = iden[axis] * transforms[axis](
-                        tick) - 0.05 * iden[(axis + 1) % 2]
-                    ticks_group.add(
-                        _make_sprite(string=value_to_string(tick),
-                                     position=tick_pos.tolist(),
-                                     size=self._tick_size))
-        return ticks_group
+        low = transform.low
+        high = transform.high
+        ticker_ = ticker.AutoLocator()
+        ticks = ticker_.tick_values(low, high)
+        string = f'<svg width=\"{width}\" height=\"{self.font_size}\">'
+        values = []
+        for tick in ticks:
+            if low <= tick <= high:
+                trans_pos = transform(tick)
+                tick_pos = trans_pos * width
+                string += (
+                    f'<text fill=\"#000000\" font-size=\"{self.font_size}\" '
+                    f'x=\"{tick_pos}\" y=\"{0.5*self.font_size}\"'
+                    'dominant-baseline=\"middle\" text-anchor=\"middle\">'
+                    f'{value_to_string(tick)}</text>')
+                values.append(trans_pos)
+        string += '</svg>'
+        return string, values
+
+    def _make_yticks(self, transform, height) -> str:
+        """
+        Create tick labels on outline edges
+        """
+        low = transform.low
+        high = transform.high
+        ticker_ = ticker.AutoLocator()
+        ticks = ticker_.tick_values(low, high)
+        string = f'<svg width=\"50px\" height=\"{height}\">'
+        values = []
+        for tick in ticks:
+            if low <= tick <= high:
+                trans_pos = transform(tick)
+                tick_pos = height - (trans_pos * height)
+                string += (
+                    f'<text fill=\"#000000\" font-size=\"{self.font_size}\" '
+                    f'x=\"20\" y=\"{tick_pos}\"'
+                    'dominant-baseline=\"middle\" text-anchor=\"right\">'
+                    f'{value_to_string(tick)}</text>')
+                values.append(trans_pos)
+        string += '</svg>'
+        return string, values
 
     def zoom(self, box):
         self._transformx.zoom(low=self._transformx.inverse(box['left']),
@@ -140,11 +195,21 @@ class Axes(p3.Group):
     def _apply_zoom(self):
         for artist in self._artists:
             artist._apply_transform()
-        self.remove(self.ticks)
-        self.ticks = self._make_ticks(
-            limits=[[self._transformx.low, self._transformx.high],
-                    [self._transformy.low, self._transformy.high], [0, 0]])
-        self.add(self.ticks)
+        # self.remove(self.ticks)
+        # self.ticks = self._make_ticks(
+        #     limits=[[self._transformx.low, self._transformx.high],
+        #             [self._transformy.low, self._transformy.high], [0, 0]])
+        # self.add(self.ticks)
+        yticklabels, yticks = self._make_yticks(transform=self._transformy,
+                                                height=self._height)
+        self._left_bar.value = yticklabels
+        xticklabels, xticks = self._make_xticks(transform=self._transformx,
+                                                width=self._width)
+        self._bottom_bar.value = xticklabels
+        self._outline.geometry.attributes[
+            'position'].array = _make_outline_array(xticks=xticks,
+                                                    yticks=yticks,
+                                                    tick_size=0.02)
 
     def reset(self):
         self._transformx.reset()
@@ -153,4 +218,10 @@ class Axes(p3.Group):
 
     def set_figure(self, fig):
         self._fig = fig
-        self._frame.material.color = self._fig.background_color
+        self._fig.camera.add(self._outline)
+        self._width = self._fig.width
+        self._height = self._fig.height
+        self._fig.left_bar.children += (self._left_bar, )
+        self._fig.bottom_bar.children += (self._bottom_bar, )
+
+        # self._frame.material.color = self._fig.background_color
--- src/matplotgl/figure.py
+++ src/matplotgl/figure.py
@@ -5,7 +5,7 @@ import numpy as np
 import pythreejs as p3
 
 
-class Figure(ipw.HBox):
+class Figure(ipw.GridBox):
 
     def __init__(self) -> None:
 
@@ -40,16 +40,18 @@ class Figure(ipw.HBox):
                                        material=self._zoom_rect_material,
                                        visible=False)
 
-        self.width = 800
-        self.height = 500
+        self.width = 700
+        self.height = 450
         # self.camera = p3.PerspectiveCamera(position=[0.0, 0, 2],
         #                                    aspect=self.width / self.height)
-        self.camera = p3.OrthographicCamera(-0.1, 1.1, 1.1, -0.1, -1, 100)
+        # self.camera = p3.OrthographicCamera(-0.1, 1.1, 1.1, -0.1, -1, 100)
+        self.camera = p3.OrthographicCamera(-0.001, 1.0, 1.0, -0.001, -1, 100)
         self.scene = p3.Scene(children=[
             self.camera, self._background_mesh, self._zoom_rect_line
         ],
                               background=self.background_color)
-        self.controls = p3.OrbitControls(controlling=self.camera)
+        self.controls = p3.OrbitControls(controlling=self.camera,
+                                         enableZoom=False)
         self.renderer = p3.Renderer(
             camera=self.camera,
             scene=self.scene,
@@ -64,7 +66,33 @@ class Figure(ipw.HBox):
         self._zoom_mouse_down = False
         self._zoom_mouse_moved = False
 
-        super().__init__([self.toolbar, self.renderer])
+        self.toolbar.layout = ipw.Layout(grid_area='toolbar')
+        self.renderer.layout = ipw.Layout(grid_area='main')
+
+        self.left_bar = ipw.VBox(layout=ipw.Layout(grid_area='left'))
+        self.right_bar = ipw.VBox(layout=ipw.Layout(grid_area='right'))
+        self.bottom_bar = ipw.HBox(layout=ipw.Layout(grid_area='bottom'))
+        self.top_bar = ipw.HBox(layout=ipw.Layout(grid_area='top'))
+
+        # super().__init__([self.toolbar, self.left_bar,
+        #     ipw.VBox([self.top_bar, self.renderer, self.bottom_bar]), self.right_bar]),
+        #     self.bottom_bar
+        # ])
+
+        super().__init__(
+            children=[
+                self.toolbar, self.renderer, self.left_bar, self.right_bar,
+                self.bottom_bar, self.top_bar
+            ],
+            layout=ipw.Layout(
+                grid_template_rows='auto auto auto',
+                # grid_template_columns='5% 5% 85% 5%',
+                grid_template_columns='auto auto auto auto',
+                grid_template_areas='''
+            ". . top ."
+            "toolbar left main right"
+            ". . bottom ."
+            '''))
 
     def home(self, *args):
         for ax in self._axes:
@@ -124,4 +152,4 @@ class Figure(ipw.HBox):
     def add_axes(self, ax):
         self._axes.append(ax)
         ax.set_figure(self)
-        self.camera.add(ax)
+        # self.camera.add(ax)
