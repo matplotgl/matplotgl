a184f26a236e100b214099971c1db88223ea745c
Neil Vaytet <neil.vaytet@esss.se>
2023-01-08 17:03:49 +0100

    refactor renderer inside axes

--- src/matplotgl/axes.py
+++ src/matplotgl/axes.py
@@ -3,6 +3,7 @@
 
 from .transform import Transform
 from .utils import value_to_string
+from .widgets import HBar
 
 import ipywidgets as ipw
 import pythreejs as p3
@@ -10,19 +11,21 @@ from matplotlib import ticker
 import numpy as np
 from typing import List, Tuple
 
-# def _make_sprite(string: str,
-#                  position: Tuple[float, float, float],
-#                  color: str = "black",
-#                  size: float = 1.0) -> p3.Sprite:
-#     """
-#     Make a text-based sprite for axis tick.
-#     """
-#     sm = p3.SpriteMaterial(map=p3.TextTexture(string=string,
-#                                               color=color,
-#                                               size=300,
-#                                               squareTexture=False),
-#                            transparent=True)
-#     return p3.Sprite(material=sm, position=position, scale=[size, size, size])
+
+def _make_sprite(string: str,
+                 position: Tuple[float, float, float],
+                 color: str = "black",
+                 size: float = 1.0) -> p3.Sprite:
+    """
+    Make a text-based sprite for axis tick.
+    """
+    sm = p3.SpriteMaterial(map=p3.TextTexture(string=string,
+                                              color=color,
+                                              size=300,
+                                              squareTexture=False),
+                           transparent=True)
+    return p3.Sprite(material=sm, position=position, scale=[size, size, size])
+
 
 # def _make_outline_array(xticks, yticks, tick_size=0.02):
 #     x = [0]
@@ -58,32 +61,33 @@ def _make_outline(color='black'):
     return p3.Line(geometry=geom, material=material)
 
 
-# def _make_frame(color='white'):
-#     dx = 1.0
-#     x = np.array([-dx, -dx, 1 + dx, 1 + dx, 0, 0, 1, 1])
-#     y = np.array([-dx, 1 + dx, 1 + dx, -dx, 0, 1, 1, 0])
+def _make_frame(color='white'):
+    dx = 1.0
+    x = np.array([-dx, -dx, 1 + dx, 1 + dx, 0, 0, 1, 1])
+    y = np.array([-dx, 1 + dx, 1 + dx, -dx, 0, 1, 1, 0])
 
-#     vertices = np.array([x, y, np.zeros_like(x)], dtype='float32').T
+    vertices = np.array([x, y, np.zeros_like(x)], dtype='float32').T
 
-#     faces = np.asarray(
-#         [[0, 4, 1], [1, 4, 5], [1, 5, 2], [2, 5, 6], [2, 6, 3], [3, 6, 7],
-#          [0, 3, 7], [0, 7, 4]],
-#         dtype='uint16').ravel()  # We need to flatten index array
+    faces = np.asarray(
+        [[0, 4, 1], [1, 4, 5], [1, 5, 2], [2, 5, 6], [2, 6, 3], [3, 6, 7],
+         [0, 3, 7], [0, 7, 4]],
+        dtype='uint16').ravel()  # We need to flatten index array
 
-#     geom = p3.BufferGeometry(attributes=dict(
-#         position=p3.BufferAttribute(vertices, normalized=False),
-#         index=p3.BufferAttribute(faces, normalized=False),
-#     ))
+    geom = p3.BufferGeometry(attributes=dict(
+        position=p3.BufferAttribute(vertices, normalized=False),
+        index=p3.BufferAttribute(faces, normalized=False),
+    ))
 
-#     return p3.Mesh(geometry=geom,
-#                    material=p3.MeshBasicMaterial(color=color),
-#                    position=[0, 0, -1])
+    return p3.Mesh(geometry=geom,
+                   material=p3.MeshBasicMaterial(color=color),
+                   position=[0, 0, -1])
 
 
-class Axes:
+class Axes(HBar):
 
     def __init__(self) -> None:
 
+        self.background_color = "#f0f0f0"
         self.xmin = 0.0
         self.xmax = 1.0
         self.ymin = 0.0
@@ -92,32 +96,136 @@ class Axes:
         self._transformy = Transform()
         self._fig = None
         self._artists = []
-        self._width = 100
-        self._height = 100
-        self.font_size = 14
-        self._html_div_offset = 8
+        # self._width = 200
+        # self._height = 200
 
-        xticklabels = self._make_xticks(transform=self._transformx,
-                                        width=self._width)
-        yticklabels = self._make_yticks(transform=self._transformy,
-                                        height=self._height)
+        # Make background to enable box zoom
+        self._background_geometry = p3.BoxGeometry(width=2,
+                                                   height=2,
+                                                   widthSegments=1,
+                                                   heightSegments=1)
+        self._background_material = p3.MeshBasicMaterial(
+            color=self.background_color, side='DoubleSide')
+        self._background_mesh = p3.Mesh(geometry=self._background_geometry,
+                                        material=self._background_material,
+                                        position=(0, 0, -100))
+
+        self._zoom_down_picker = p3.Picker(controlling=self._background_mesh,
+                                           event='mousedown')
+        self._zoom_up_picker = p3.Picker(controlling=self._background_mesh,
+                                         event='mouseup')
+        self._zoom_move_picker = p3.Picker(controlling=self._background_mesh,
+                                           event='mousemove')
+
+        self._zoom_rect_geometry = p3.BufferGeometry(
+            attributes={
+                'position': p3.BufferAttribute(array=np.zeros((5, 3))),
+            })
+        self._zoom_rect_material = p3.LineBasicMaterial(color='black',
+                                                        linewidth=1)
+        self._zoom_rect_line = p3.Line(geometry=self._zoom_rect_geometry,
+                                       material=self._zoom_rect_material,
+                                       visible=False)
+
+        # xticklabels = self._make_xticks(transform=self._transformx,
+        #                                 width=self._width)
+        # yticklabels = self._make_yticks(transform=self._transformy,
+        #                                 height=self._height)
         self._outline = _make_outline()
+        self._frame = _make_frame()
 
-        # self._left_bar = ipw.HTML(yticklabels,
-        #                           layout={'height': f'{self._height}px'})
-        self._left_bar = ipw.HTML(yticklabels)
-        self._right_bar = ipw.HTML()
-        self._bottom_bar = ipw.HTML(xticklabels)
-        # self._bottom_bar = ipw.Button(icon='home', layout={'width': '600px'})
-        self._top_bar = ipw.HTML()
-        # self._frame = _make_frame()
+        # self.width = 600
+        # self.height = 400
+        # self.camera = p3.PerspectiveCamera(position=[0.0, 0, 2],
+        #                                    aspect=self.width / self.height)
+        self.camera = p3.OrthographicCamera(-0.1, 1.1, 1.1, -0.1, -1, 100)
+        # self.camera = p3.OrthographicCamera(-0.001, 1.0, 1.0, -0.001, -1, 100)
+        self.scene = p3.Scene(children=[
+            self.camera, self._background_mesh, self._outline, self._frame,
+            self._zoom_rect_line
+        ],
+                              background=self.background_color)
+        self.controls = p3.OrbitControls(controlling=self.camera,
+                                         enableZoom=False,
+                                         enablePan=False)
+        self.renderer = p3.Renderer(
+            camera=self.camera,
+            scene=self.scene,
+            controls=[self.controls],
+            width=200,
+            height=200,
+            # antialiasing=True
+        )
 
-        # limits = [[self.xmin, self.xmax], [self.ymin, self.ymax], [0, 0]]
-        # self.ticks = self._make_ticks(limits=limits)
+        self._zoom_mouse_down = False
+        self._zoom_mouse_moved = False
 
-        # super().__init__()
+        # # self._left_bar = ipw.HTML(yticklabels,
+        # #                           layout={'height': f'{self._height}px'})
+        # self._left_bar = ipw.HTML(yticklabels)
+        # self._right_bar = ipw.HTML()
+        # self._bottom_bar = ipw.HTML(xticklabels)
+        # # self._bottom_bar = ipw.Button(icon='home', layout={'width': '600px'})
+        # self._top_bar = ipw.HTML()
+        # # self._frame = _make_frame()
+
+        # # limits = [[self.xmin, self.xmax], [self.ymin, self.ymax], [0, 0]]
+        # # self.ticks = self._make_ticks(limits=limits)
+
+        super().__init__([self.renderer])
         # for obj in (self._outline, self.ticks, self._frame):
         #     self.add(obj)
+
+    def on_mouse_down(self, change):
+        self._zoom_mouse_down = True
+        x, y, z = change['new']
+        self._zoom_rect_line.geometry.attributes['position'].array = np.array(
+            [[x, x, x, x, x], [y, y, y, y, y], [0, 0, 0, 0, 0]]).T
+        self._zoom_rect_line.visible = True
+        # print(x, y, z, self._zoom_rect_line.visible, self.camera.near,
+        #       self.camera.far)
+
+    def on_mouse_up(self, *ignored):
+        if self._zoom_mouse_down:
+            self._zoom_mouse_down = False
+            self._zoom_rect_line.visible = False
+            if self._zoom_mouse_moved:
+                array = self._zoom_rect_line.geometry.attributes[
+                    'position'].array
+                self.zoom({
+                    'left': array[:, 0].min(),
+                    'right': array[:, 0].max(),
+                    'bottom': array[:, 1].min(),
+                    'top': array[:, 1].max()
+                })
+                self._zoom_mouse_moved = False
+
+    def on_mouse_move(self, change):
+        if self._zoom_mouse_down:
+            self._zoom_mouse_moved = True
+            x, y, z = change['new']
+            new_pos = self._zoom_rect_line.geometry.attributes[
+                'position'].array.copy()
+            new_pos[2:4, 0] = x
+            new_pos[1:3, 1] = y
+            self._zoom_rect_line.geometry.attributes[
+                'position'].array = new_pos
+
+    @property
+    def width(self):
+        return self.renderer.width
+
+    @width.setter
+    def width(self, w):
+        self.renderer.width = w
+
+    @property
+    def height(self):
+        return self.renderer.height
+
+    @height.setter
+    def height(self, h):
+        self.renderer.height = h
 
     def plot(self, *args, **kwargs):
         from .plot import plot as p
@@ -141,7 +249,7 @@ class Axes:
 
     def add_artist(self, artist):
         self._artists.append(artist)
-        self._fig.scene.add(artist._line)
+        self.scene.add(artist._line)
 
     def get_figure(self):
         return self._fig
@@ -226,17 +334,17 @@ class Axes:
         #     limits=[[self._transformx.low, self._transformx.high],
         #             [self._transformy.low, self._transformy.high], [0, 0]])
         # self.add(self.ticks)
-        print('_apply_zoom', self._height)
-        yticklabels = self._make_yticks(transform=self._transformy,
-                                        height=self._height)
-        self._left_bar.value = yticklabels
-        xticklabels = self._make_xticks(transform=self._transformx,
-                                        width=self._width)
-        self._bottom_bar.value = xticklabels
-        # self._outline.geometry.attributes[
-        #     'position'].array = _make_outline_array(xticks=xticks,
-        #                                             yticks=yticks,
-        #                                             tick_size=0.02)
+        # print('_apply_zoom', self._height)
+        # yticklabels = self._make_yticks(transform=self._transformy,
+        #                                 height=self._height)
+        # self._left_bar.value = yticklabels
+        # xticklabels = self._make_xticks(transform=self._transformx,
+        #                                 width=self._width)
+        # self._bottom_bar.value = xticklabels
+        # # self._outline.geometry.attributes[
+        # #     'position'].array = _make_outline_array(xticks=xticks,
+        # #                                             yticks=yticks,
+        # #                                             tick_size=0.02)
 
     def reset(self):
         self._transformx.reset()
@@ -245,11 +353,11 @@ class Axes:
 
     def set_figure(self, fig):
         self._fig = fig
-        self._fig.camera.add(self._outline)
-        self._width = self._fig.width
-        self._height = self._fig.height
+        # self._fig.camera.add(self._outline)
+        self.width = self._fig.width
+        self.height = self._fig.height
         # self._left_bar.layout = {'height': f'{self._height-2}px'}
-        self._fig.left_bar.children += (self._left_bar, )
-        self._fig.bottom_bar.children += (self._bottom_bar, )
+        # self._fig.left_bar.children += (self._left_bar, )
+        # self._fig.bottom_bar.children += (self._bottom_bar, )
 
         # self._frame.material.color = self._fig.background_color
--- src/matplotgl/figure.py
+++ src/matplotgl/figure.py
@@ -1,158 +1,124 @@
 from .toolbar import Toolbar
+from .widgets import HBar
 
 import ipywidgets as ipw
 import numpy as np
 import pythreejs as p3
 
 
-class Figure(ipw.GridBox):
+class Figure(HBar):
 
-    def __init__(self) -> None:
+    def __init__(self, figsize=(6., 4.)) -> None:
 
-        self.background_color = "#f0f0f0"
-        self._axes = []
+        self.axes = []
+        self._dpi = 96
+        self.width = figsize[0] * self._dpi
+        self.height = figsize[1] * self._dpi
 
-        # Make background to enable box zoom
-        self._background_geometry = p3.BoxGeometry(width=200,
-                                                   height=200,
-                                                   widthSegments=1,
-                                                   heightSegments=1)
-        self._background_material = p3.MeshBasicMaterial(
-            color=self.background_color, side='DoubleSide')
-        self._background_mesh = p3.Mesh(geometry=self._background_geometry,
-                                        material=self._background_material,
-                                        position=(0, 0, -100))
+        # # Make background to enable box zoom
+        # self._background_geometry = p3.BoxGeometry(width=200,
+        #                                            height=200,
+        #                                            widthSegments=1,
+        #                                            heightSegments=1)
+        # self._background_material = p3.MeshBasicMaterial(
+        #     color=self.background_color, side='DoubleSide')
+        # self._background_mesh = p3.Mesh(geometry=self._background_geometry,
+        #                                 material=self._background_material,
+        #                                 position=(0, 0, -100))
 
-        self._zoom_down_picker = p3.Picker(controlling=self._background_mesh,
-                                           event='mousedown')
-        self._zoom_up_picker = p3.Picker(controlling=self._background_mesh,
-                                         event='mouseup')
-        self._zoom_move_picker = p3.Picker(controlling=self._background_mesh,
-                                           event='mousemove')
+        # self._zoom_down_picker = p3.Picker(controlling=self._background_mesh,
+        #                                    event='mousedown')
+        # self._zoom_up_picker = p3.Picker(controlling=self._background_mesh,
+        #                                  event='mouseup')
+        # self._zoom_move_picker = p3.Picker(controlling=self._background_mesh,
+        #                                    event='mousemove')
 
-        self._zoom_rect_geometry = p3.BufferGeometry(
-            attributes={
-                'position': p3.BufferAttribute(array=np.zeros((5, 3))),
-            })
-        self._zoom_rect_material = p3.LineBasicMaterial(color='black',
-                                                        linewidth=1)
-        self._zoom_rect_line = p3.Line(geometry=self._zoom_rect_geometry,
-                                       material=self._zoom_rect_material,
-                                       visible=False)
+        # self._zoom_rect_geometry = p3.BufferGeometry(
+        #     attributes={
+        #         'position': p3.BufferAttribute(array=np.zeros((5, 3))),
+        #     })
+        # self._zoom_rect_material = p3.LineBasicMaterial(color='black',
+        #                                                 linewidth=1)
+        # self._zoom_rect_line = p3.Line(geometry=self._zoom_rect_geometry,
+        #                                material=self._zoom_rect_material,
+        #                                visible=False)
 
-        self.width = 600
-        self.height = 400
-        # self.camera = p3.PerspectiveCamera(position=[0.0, 0, 2],
-        #                                    aspect=self.width / self.height)
-        # self.camera = p3.OrthographicCamera(-0.1, 1.1, 1.1, -0.1, -1, 100)
-        self.camera = p3.OrthographicCamera(-0.001, 1.0, 1.0, -0.001, -1, 100)
-        self.scene = p3.Scene(children=[
-            self.camera, self._background_mesh, self._zoom_rect_line
-        ],
-                              background=self.background_color)
-        self.controls = p3.OrbitControls(controlling=self.camera,
-                                         enableZoom=False,
-                                         enablePan=False)
-        self.renderer = p3.Renderer(
-            camera=self.camera,
-            scene=self.scene,
-            controls=[self.controls],
-            width=self.width,
-            height=self.height,
-            # antialiasing=True
-        )
+        # self.width = 600
+        # self.height = 400
+        # # self.camera = p3.PerspectiveCamera(position=[0.0, 0, 2],
+        # #                                    aspect=self.width / self.height)
+        # # self.camera = p3.OrthographicCamera(-0.1, 1.1, 1.1, -0.1, -1, 100)
+        # self.camera = p3.OrthographicCamera(-0.001, 1.0, 1.0, -0.001, -1, 100)
+        # self.scene = p3.Scene(children=[
+        #     self.camera, self._background_mesh, self._zoom_rect_line
+        # ],
+        #                       background=self.background_color)
+        # self.controls = p3.OrbitControls(controlling=self.camera,
+        #                                  enableZoom=False,
+        #                                  enablePan=False)
+        # self.renderer = p3.Renderer(
+        #     camera=self.camera,
+        #     scene=self.scene,
+        #     controls=[self.controls],
+        #     width=self.width,
+        #     height=self.height,
+        #     # antialiasing=True
+        # )
         self.toolbar = Toolbar()
         self.toolbar._home.on_click(self.home)
         self.toolbar._zoom.observe(self.toggle_pickers, names='value')
-        self._zoom_mouse_down = False
-        self._zoom_mouse_moved = False
+        # self._zoom_mouse_down = False
+        # self._zoom_mouse_moved = False
 
-        self.toolbar.layout = ipw.Layout(grid_area='toolbar')
-        self.renderer.layout = ipw.Layout(grid_area='main')
+        # self.toolbar.layout = ipw.Layout(grid_area='toolbar')
+        # self.renderer.layout = ipw.Layout(grid_area='main')
 
-        self.left_bar = ipw.VBox(layout=ipw.Layout(grid_area='left'))
-        self.right_bar = ipw.VBox(layout=ipw.Layout(grid_area='right'))
-        self.bottom_bar = ipw.HBox(layout=ipw.Layout(grid_area='bottom'))
-        self.top_bar = ipw.HBox(layout=ipw.Layout(grid_area='top'))
+        # self.left_bar = ipw.VBox(layout=ipw.Layout(grid_area='left'))
+        # self.right_bar = ipw.VBox(layout=ipw.Layout(grid_area='right'))
+        # self.bottom_bar = ipw.HBox(layout=ipw.Layout(grid_area='bottom'))
+        # self.top_bar = ipw.HBox(layout=ipw.Layout(grid_area='top'))
 
-        # super().__init__([self.toolbar, self.left_bar,
-        #     ipw.VBox([self.top_bar, self.renderer, self.bottom_bar]), self.right_bar]),
-        #     self.bottom_bar
-        # ])
+        super().__init__([self.toolbar])
 
-        super().__init__(
-            children=[
-                self.toolbar, self.renderer, self.left_bar, self.right_bar,
-                self.bottom_bar, self.top_bar
-            ],
-            layout=ipw.Layout(
-                grid_template_rows=f'auto {self.height}px 40px',
-                # grid_template_columns='5% 5% 85% 5%',
-                # grid_template_columns='40px 42px auto 0px',
-                grid_template_columns='40px 65px auto 0px',
-                grid_template_areas='''
-            ". . top ."
-            "toolbar left main right"
-            ". . bottom ."
-            ''',
-                grid_gap='0px 0px'))
+        # super().__init__(
+        #     children=[
+        #         self.toolbar, self.renderer, self.left_bar, self.right_bar,
+        #         self.bottom_bar, self.top_bar
+        #     ],
+        #     layout=ipw.Layout(
+        #         grid_template_rows=f'auto {self.height}px 40px',
+        #         # grid_template_columns='5% 5% 85% 5%',
+        #         # grid_template_columns='40px 42px auto 0px',
+        #         grid_template_columns='40px 65px auto 0px',
+        #         grid_template_areas='''
+        #     ". . top ."
+        #     "toolbar left main right"
+        #     ". . bottom ."
+        #     ''',
+        #         grid_gap='0px 0px'))
 
     def home(self, *args):
-        for ax in self._axes:
+        for ax in self.axes:
             ax.reset()
 
     def toggle_pickers(self, change):
-        if change['new']:
-            self._zoom_down_picker.observe(self.on_mouse_down, names=['point'])
-            self._zoom_up_picker.observe(self.on_mouse_up, names=['point'])
-            self._zoom_move_picker.observe(self.on_mouse_move, names=['point'])
-            self.renderer.controls = [
-                self.controls, self._zoom_down_picker, self._zoom_up_picker,
-                self._zoom_move_picker
-            ]
-        else:
-            self._zoom_down_picker.unobserve_all()
-            self._zoom_up_picker.unobserve_all()
-            self._zoom_move_picker.unobserve_all()
-            self.renderer.controls = [self.controls]
-
-    def on_mouse_down(self, change):
-        self._zoom_mouse_down = True
-        x, y, z = change['new']
-        self._zoom_rect_line.geometry.attributes['position'].array = np.array(
-            [[x, x, x, x, x], [y, y, y, y, y], [0, 0, 0, 0, 0]]).T
-        self._zoom_rect_line.visible = True
-        print(x, y, z, self._zoom_rect_line.visible, self.camera.near,
-              self.camera.far)
-
-    def on_mouse_up(self, *ignored):
-        if self._zoom_mouse_down:
-            self._zoom_mouse_down = False
-            self._zoom_rect_line.visible = False
-            if self._zoom_mouse_moved:
-                array = self._zoom_rect_line.geometry.attributes[
-                    'position'].array
-                for ax in self._axes:
-                    ax.zoom({
-                        'left': array[:, 0].min(),
-                        'right': array[:, 0].max(),
-                        'bottom': array[:, 1].min(),
-                        'top': array[:, 1].max()
-                    })
-                self._zoom_mouse_moved = False
-
-    def on_mouse_move(self, change):
-        if self._zoom_mouse_down:
-            self._zoom_mouse_moved = True
-            x, y, z = change['new']
-            new_pos = self._zoom_rect_line.geometry.attributes[
-                'position'].array.copy()
-            new_pos[2:4, 0] = x
-            new_pos[1:3, 1] = y
-            self._zoom_rect_line.geometry.attributes[
-                'position'].array = new_pos
+        for ax in self.axes:
+            if change['new']:
+                ax._zoom_down_picker.observe(ax.on_mouse_down, names=['point'])
+                ax._zoom_up_picker.observe(ax.on_mouse_up, names=['point'])
+                ax._zoom_move_picker.observe(ax.on_mouse_move, names=['point'])
+                ax.renderer.controls = [
+                    ax.controls, ax._zoom_down_picker, ax._zoom_up_picker,
+                    ax._zoom_move_picker
+                ]
+            else:
+                ax._zoom_down_picker.unobserve_all()
+                ax._zoom_up_picker.unobserve_all()
+                ax._zoom_move_picker.unobserve_all()
+                ax.renderer.controls = [ax.controls]
 
     def add_axes(self, ax):
-        self._axes.append(ax)
+        self.axes.append(ax)
         ax.set_figure(self)
+        self.add(ax)
         # self.camera.add(ax)
