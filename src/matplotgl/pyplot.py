1d12266fbeb4ca800f039de7756df08c894a001f
Neil Vaytet <neil.vaytet@ess.eu>
2025-10-31 18:00:43 +0100

    small cleanup

--- src/matplotgl/pyplot.py
+++ src/matplotgl/pyplot.py
@@ -15,8 +15,6 @@ matplotlib.use("Agg")  # Headless backend
 
 def subplots(nrows=1, ncols=1, **kwargs):
     mpl_figure = MplFigure(**kwargs)
-    # for i in range(nrows * ncols):
-    #     ax = mpl_figure.add_subplot(nrows, ncols, i + 1)
 
     fig = Figure(nrows=nrows, ncols=ncols, **kwargs)
     axs = []
@@ -26,17 +24,9 @@ def subplots(nrows=1, ncols=1, **kwargs):
             mplax = mpl_figure.add_subplot(nrows, ncols, j * nrows + i + 1)
             ax = Axes(ax=mplax, figure=fig)
             fig.axes.append(ax)
-            # ax.set_figure(fig, width=fig.width / ncols, height=fig.height / nrows)
             column.append(ax)
         axs.append(column)
         fig.add(VBar(column))
-
-    # axs = [[Axes() for i in range(nrows)] for j in range(ncols)]
-    # for col in axs:
-    #     for ax in col:
-    #         fig.axes.append(ax)
-    #         ax.set_figure(fig, width=fig.width / ncols, height=fig.height / nrows)
-    #     fig.add(VBar(col))
 
     if nrows + ncols == 2:
         out = axs[0][0]
@@ -49,8 +39,4 @@ def subplots(nrows=1, ncols=1, **kwargs):
     return fig, out
 
 
-__all__ = [
-    "Axes",
-    "Figure",
-    "subplots",
-]
+__all__ = ["Axes", "Figure", "subplots"]
